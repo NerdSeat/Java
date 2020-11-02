0b0f6ab97f88e65e6b810b25b5a4e9131a3c9cfa
Blisterface <josephblister@gamil.com>
2020-11-02 10:36:48 +0200

    Strings Algorithms

---
+++ chap1/ex1.java
@@ -0,0 +1,20 @@
+class strings
+{
+    public static void main(String[] args)
+    {
+        String[] words = {"funk","chunk","furry","baconator"};
+        //starts with
+        for(String w: words)
+        {
+            if(w.startsWith("fu"))
+                System.out.println(w + " starts with fu");
+        }
+        //ends with
+        for(String w: words)
+        {
+            if(w.endsWith("unk"))
+                System.out.println(w + " ends with unk");
+            System.out.println(w.indexOf('u'));
+        }
+    }
+}
