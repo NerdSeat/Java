fd091e33ad4daf57bc1d76c64abbeef9c7f8da50
NerdSeat <josephblister@gamil.com>
2020-12-14 22:32:54 +0200

    bubble sort algorithm

---
+++ Sorting/src/bubbleSort.java
@@ -0,0 +1,30 @@
+public class bubbleSort{
+
+
+public static void swap(int array[],int i,int j)
+{
+    if(i==j){
+        return;
+    }
+    int temp = array[i];
+    array[i]=array[j];
+    array[j]=temp;
+}
+
+public static void main(String[] args) {
+    int [] intArray = {20,35,-15,7,55,1,-22};
+
+    for(int unsortedIndex = intArray.length-1;unsortedIndex>0;unsortedIndex--)
+    {
+        for(int i=0;i<unsortedIndex;i++)
+        {
+            if(intArray[i]>intArray[i+1])
+                swap(intArray, i, i+1);
+        }
+    }
+    for(int i=0;i<intArray.length;i++)
+        System.out.print(intArray[i]+"\t");
+    
+}
+
+}
